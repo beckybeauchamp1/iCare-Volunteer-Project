6e234b4239fcd8143e854820fb13b50e5c909eb7
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-04-13 07:27:37 -0400

    example

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module
-  
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -24,4 +23,5 @@ module
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
-end
+
+  # That extra end was throwing me an error, I couldn't start my server. 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  # I would have some type of "home" route so it directs users to a page
+  get '' => 'events#index'
   resources :events
   resources :stories
 end
